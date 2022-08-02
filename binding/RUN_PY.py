3e7b9f3d992691f299f06718dda2087b7ccd81be
sbaldu <simone.balducci00@gmail.com>
2022-08-02 11:20:47 +0200

    Starting to work on the binding

---
+++ binding/RUN_PY.py
@@ -0,0 +1,7 @@
+import CLUE
+
+inputFileName = "data/input/aniso_1000.csv"
+outputFileName = "data/output/FIRST_TEST.csv"
+
+CLUE.algo(inputFileName, outputFileName,
+          20, 25, 2, 0, 10, 1);
