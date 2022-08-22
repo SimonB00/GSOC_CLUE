0442c9c285a553267366c0611987b2f19df3a2c0
SimonB00 <simone.balducci00@gmail.com>
2022-08-22 14:30:22 +0200

    Updated input plotter

--- plotInput.py
+++ plotInput.py
@@ -1,24 +1,40 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import glob
 
-file = input('What file do you want to plot? ')
+def getFolder(file):
+	reversed = file[::-1]
+	
+	folder = ''
+	begin = False
+	for char in reversed:
+		if char == '/':
+			begin = True
+		if begin:
+			folder += char
+	
+	return folder[::-1]
+
+fileNotFound = True
+file = ''
+while fileNotFound:
+	file = input('What file do you want to plot? ')
+	
+	folder = getFolder(file)
+	files = glob.glob(folder)
+	if file in files:
+		fileNotFound = False
+	else:
+		print('File not found')
+
 Ndim = int(input('Number of dimensions? '))
-
 df = pd.read_csv(file,header=None)
-len_ = len(df[0].values.tolist())
 
-data = []
-for i in range(Ndim):
-	data.append([])
-
-for i in range(len_):
-	for j in range(Ndim):
-		data[j].append(df[j][i])
 if Ndim == 2:
-	plt.scatter(x=data[0],y=data[1],s=1)
+	plt.scatter(x=df[0],y=df[1],s=1)
 	plt.show()
 if Ndim == 3:
 	fig = plt.figure()
 	ax = fig.add_subplot(projection='3d')
-	ax.scatter(data[0],data[1],data[2])
+	ax.scatter(df[0],df[1],df[2])
 	plt.show()
