1f5d34214fca38c0ba710df981fdd282c30c2646
SimonB00 <simone.balducci00@gmail.com>
2022-08-29 14:00:51 +0200

    Creating the library class

---
+++ pyClueLibrary/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="",
+    version=VERSION,
+    author="",
+    author_email="<>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[
+        "Development Status :: ",
+        "Intended Audience :: ",
+        "Programming Language :: ",
+        "Operating System :: ",
+        "Operating System :: ",
+        "Operating System :: ",
+    ]
+)
