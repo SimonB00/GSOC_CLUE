0986b3548b791612badd879c8a5191814be302db
sbaldu <simone.balducci00@gmail.com>
2022-08-29 15:19:14 +0200

    Working on the library class

--- pyClueLibrary/pyCLUE/pyCLUE.py
+++ pyClueLibrary/pyCLUE/pyCLUE.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 
 def getInputName(inputFileName):
 	inputFileName = inputFileName[::-1]
@@ -18,7 +19,18 @@ def getInputName(inputFileName):
 
 def createOutputName(inputFileName_, pathOutput, parameters):	
 	# the input name should be like pathToInput/fileName.csv
-	inputName = 
+	inputName = getInputName(inputFileName_)
+	outputFileName = pathOutput + inputName
+
+	for par in parameters:
+		outputFileName += '_' + str(parameters[par])
+	outputFileName += '.csv'
+	
+	return outputFileName
+
+def chooseClusterer(Ndim_):
+	if Ndim_ == 2:
+		return pyCLUE.clusteringAlgo2(parameters['dc'],parameters['rhoc'],parameters['outlier'],parameters['ppBin'])
 
 class clusterer:
 	def __init__(self, inputFileName, dc, rhoc, outlier, ppBin): 
@@ -31,3 +43,40 @@ class clusterer:
 		pass
 	def clusterPlotter():
 		pass
+	def readData(self):
+		print('Start loading points')
+
+		df = pd.read_csv(self.inputFileName, header=None)
+		self.Ndim = len(df.columns) - 1 
+
+		self.coords = []
+		for j in range(self.Ndim):
+			coords.append(list(df[j]))
+		self.weight = list(df[self.Ndim])
+
+		print('Finished loading points')
+	
+	def chooseClusterer(self):
+		if self.Ndim == 2:
+			return pyCLUE.clusteringAlgo2(self.dc,self.rhoc,self.outlier,self.ppBin)
+		if self.Ndim == 3:
+			return pyCLUE.clusteringAlgo3(self.dc,self.rhoc,self.outlier,self.ppBin)
+		if self.Ndim == 4:
+			return pyCLUE.clusteringAlgo4(self.dc,self.rhoc,self.outlier,self.ppBin)
+		if self.Ndim == 5:
+			return pyCLUE.clusteringAlgo5(self.dc,self.rhoc,self.outlier,self.ppBin)
+		if self.Ndim == 6:
+			return pyCLUE.clusteringAlgo6(self.dc,self.rhoc,self.outlier,self.ppBin)
+		if self.Ndim == 7:
+			return pyCLUE.clusteringAlgo7(self.dc,self.rhoc,self.outlier,self.ppBin)
+		if self.Ndim == 8:
+			return pyCLUE.clusteringAlgo8(self.dc,self.rhoc,self.outlier,self.ppBin)
+		if self.Ndim == 9:
+			return pyCLUE.clusteringAlgo9(self.dc,self.rhoc,self.outlier,self.ppBin)
+		if self.Ndim == 10:
+			return pyCLUE.clusteringAlgo10(self.dc,self.rhoc,self.outlier,self.ppBin)
+	def runCLUE(self):
+		print('Start running CLUE')
+		start = time.time_ns()
+
+		clusterer = chooseClusterer(self.Ndim)
