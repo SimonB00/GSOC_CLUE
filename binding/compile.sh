c6ec1330d558e709057b4a93c7caec5108351ef1
sbaldu <simone.balducci00@gmail.com>
2022-09-21 14:11:53 +0200

    Fixed compiling script

--- binding/compile.sh
+++ binding/compile.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/bash
+#
+rm *.so
 
 g++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) binding.cc -o clusteringAlgo$(python3-config --extension-suffix)
 
-cp clusteringAlgo.cpython-310-x86_64-linux-gnu.so ../pyClueLibrary/pyCLUE/
+cp *.so ../pyClueLibrary/pyCLUE/
